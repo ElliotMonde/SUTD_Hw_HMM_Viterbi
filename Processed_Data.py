aacd8c8bc39a58052fd0e70dd0716cd4d1ab2559
ElliotMonde <elliotphua@gmail.com>
2025-04-21 02:11:41 +0800

    Fix smooth_emission mle.

--- Processed_Data.py
+++ Processed_Data.py
@@ -1,7 +1,8 @@
 import os
 from pathlib import Path
 
-def process_data(filePath):
+
+def open_filepath(filePath):
     try:
         abs_path = Path(os.path.abspath(filePath))
         with open(abs_path, "r") as file:
@@ -11,39 +12,40 @@ def process_data(filePath):
         print(f"Current working directory: {os.getcwd()}")
         return None
 
-unprocessedTrain = process_data("EN/train")
-unprocessedDevIn = process_data("EN/dev.in")
-unprocessedDevOut = process_data("EN/dev.out")
-ProcessedTrain = []
-ProcessedDevIn = []
-ProcessedDevOut = []
-
-for line1 in unprocessedTrain:
-    if len(line1)!=0:
-        if line1 != '\n':
-            temp = line1.rstrip().split(" ")
-            value , tag = temp[0], temp[1]
-        else:
-            value, tag = "", "START"
-        ProcessedTrain.append([value,tag])
-        count+=1
-
-for line2 in unprocessedDevIn:
-    if len(line2)!=0:
-        temp = line2.rstrip().split(" ")
-        ProcessedDevIn.append(temp)
-        count+=1
-
-for line3 in unprocessedDevOut:
-    if len(line3)!=0:
-        temp = line3.rstrip().split(" ")
-        value , tag = temp[0], temp[1]
-        ProcessedTrain.append([value,tag])
 
 def process_all_data():
+
+    unprocessedTrain = open_filepath("EN/train")
+    unprocessedDevIn = open_filepath("EN/dev.in")
+    unprocessedDevOut = open_filepath("EN/dev.out")
+    ProcessedTrain = []
+    ProcessedDevIn = []
+    ProcessedDevOut = []
+
+    START_S = "START"
+
+    for line in unprocessedTrain:
+        value, tag = "", START_S
+        if len(line) != 0:
+            temp = line.rstrip().split(" ")
+            value, tag = temp[0], temp[1]
+        ProcessedTrain.append((value, tag))
+
+    for line in unprocessedDevIn:
+        temp = "\n"
+        if len(line) != 0:
+            temp = line.rstrip().split(" ")
+        ProcessedDevIn.append(temp)
+
+    for line in unprocessedDevOut:
+        value, tag = "", START_S
+        if len(line) != 0:
+            temp = line.rstrip().split(" ")
+            value, tag = temp[0], temp[1]
+        ProcessedTrain.append((value, tag))
+
     return {
         "ProcessedTrain": ProcessedTrain,
         "ProcessedDevIn": ProcessedDevIn,
-        "ProcessedDevOut": ProcessedDevOut
+        "ProcessedDevOut": ProcessedDevOut,
     }
-
